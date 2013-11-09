676ae209d79c10ace3fba6a60cf3861800830ab9
Garrett Hoofman <garrett.hoofman@opifexentertainment.com>
2013-11-08 23:25:26 -0500

    Added all of it! nom nom nom

---
+++ src/lib/documents/volunteer.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var volunteerSchema = mongoose.Schema({
+  // Contact Information
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  phone: String,
+  // Address Information
+  address: {
+    line1 : { type: String },
+    line2 : { type: String },
+    zip : { type: Number },
+    city : { type: String },
+    state : { type: String }
+  },
+  // Volunteer Skills ex: Nurse, Pyschiatrist, CPR, Juggling
+  skills: [String],
+  // Allergies (May be HIPPA concerns with this, could be removed)
+  restrictions: [String],
+  // Based on ISO-639-2  ( http://www.loc.gov/standards/iso639-2/php/code_list.php )
+  languages: [String],
+  availability: [Date],
+  homeVisitLiason: Boolean
+});
+
+module.exports = mongoose.model('Volunteer', volunteerSchema);
