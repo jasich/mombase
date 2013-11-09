39a9910222fed0d32790d3b9b00bae70b075cff9
Brian Scaturro <scaturrob@gmail.com>
2013-11-09 10:49:56 -0500

    include passport and supertesT

--- src/api/app.js
+++ src/api/app.js
@@ -30,7 +30,9 @@ if ('development' == app.get('env')) {
 
 app.get('/api', routes.index);
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/api/users/login', user.login);
+
+exports.app = app;
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
