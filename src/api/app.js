a504d22766e14e7286e4d98e5287ff61b230fc3f
MRSICH <jasich@gmail.com>
2013-11-09 14:31:04 -0500

    fixed stuff

--- src/api/app.js
+++ src/api/app.js
@@ -23,6 +23,7 @@ app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
+app.use(express.bodyParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
