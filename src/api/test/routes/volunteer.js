3d9a630217269f820d639573103d2d737f0565c7
Garrett Hoofman <garrett.hoofman@opifexentertainment.com>
2013-11-09 17:37:51 -0500

    Volunteer tests

--- src/api/test/routes/volunteer.js
+++ src/api/test/routes/volunteer.js
@@ -32,5 +32,46 @@ describe('POST /api/volunteers', function() {
       .expect(500, done);
   });
 
+  it('should search for volunteers', function(done) {
+    request(app)
+      .post('/api/volunteers')
+      .set('Accept', 'application/json')
+      .send({firstName:'search', lastName: 'user', email: 'search@email.com'})
+      .expect(200)
+      .end( function(err, res) {
+        if(err) return done(err);
+        assert.ok(res.res.body.firstName);
+        assert.ok(res.res.body.lastName);
+        request(app)
+          .post('/api/volunteers/search')
+          .set('Accept', 'application/json')
+          .send({})
+          .expect(200)
+          .end( function(err, res) {
+            if(err) return done(err);
+            assert( res.body.length == 2 );
+            assert( res.body[0].firstName == 'test' );
+            assert( res.body[1].firstName == 'search' );
+            done();
+          });
+      });
+  });
+
+
+  it('should search for volunteers with sorting', function(done) {
+    request(app)
+      .post('/api/volunteers/search')
+      .set('Accept', 'application/json')
+      .send({'sort.firstName':'1'})
+      .expect(200)
+      .end( function(err, res) {
+        if(err) return done(err);
+        assert( res.body.length == 2 );
+        assert( res.body[0].firstName == 'search' );
+        assert( res.body[1].firstName == 'test' );
+        done();
+      });
+    });
+
   
 });
