a2a181c15b354e315c6d95423ce918c331c37014
vagrant <vagrant@precise64.(none)>
2013-11-10 01:56:29 +0000

    user create route

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -2,6 +2,9 @@ var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
   , User = require('../../lib/documents/user');
 
+/**
+ * Passport strategy configuration
+ */
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ email: username }, function(err, user) {
@@ -45,5 +48,9 @@ exports.login = function(req, res, next) {
  * Create user
  */
 exports.create = function(req,res) {
-
+  (new User(req.body)).save(function(err, user) {
+    if (err) return res.send(400).send();
+    user.hash = undefined;
+    res.send(200, user);
+  });
 };
