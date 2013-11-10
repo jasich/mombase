527c19d54da9c5c8706a6fc000425c238af8e051
MRSICH <jasich@gmail.com>
2013-11-09 20:26:18 -0500

    Matches for all

--- src/web/app/scripts/controllers/match.js
+++ src/web/app/scripts/controllers/match.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('webApp')
-  .controller('MatchCtrl', function ($scope, GeoLocation) {
-	var promise = GeoLocation.GetLatLong({address: '720 E Fulton St. Grand Rapids, Michigan 49503'});
-	promise.then(
-		function(e){
-			alert('winner: ' + e);
-		},
-		function(e){
-			alert('Failure: ' + e);
-		}
-	);	
+  .controller('MatchCtrl', function ($scope, $routeParams, GeoLocation, Mother) {
+//	var promise = GeoLocation.GetLatLong({address: '720 E Fulton St. Grand Rapids, Michigan 49503'});
+//	promise.then(
+//		function(e){
+//			alert('winner: ' + e);
+//      console.dir(e);
+//		},
+//		function(e){
+//			alert('Failure: ' + e);
+//		}
+//	);
+
+    Mother.get({id: $routeParams.id}, function(data) {
+      console.dir(data);
+      $scope.mom = data;
+    });
 });
