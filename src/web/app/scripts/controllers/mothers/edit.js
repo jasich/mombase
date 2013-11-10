744cfc2c1744d796cc5393b38bc27633cea12d00
MRSICH <jasich@gmail.com>
2013-11-10 09:22:32 -0500

    Updates for Mother editing.

--- src/web/app/scripts/controllers/mothers/edit.js
+++ src/web/app/scripts/controllers/mothers/edit.js
@@ -1,55 +1,18 @@
 'use strict';
 
 angular.module('webApp')
-  .controller('MothersEditCtrl', function ($scope, $location, UsStates, AvailabilityCodes, LanguageCodes, Mother) {
+  .controller('MothersEditCtrl', function ($rootScope, $scope, $location, $routeParams, UsStates, AvailabilityCodes, LanguageCodes, Mother) {
     $scope.states = UsStates;
     $scope.availabilityCodes = AvailabilityCodes;
     $scope.langCodes = LanguageCodes;
 
-    $scope.mother = {
-      "__v": 16,
-      "_id": "527ee220c2f63cc762000006",
-      "email": "pam@email.com",
-      "firstName": "Pam",
-      "lastName": "Smith",
-      "primaryVolunteer": "",
-      "visits": [],
-      "volunteers": [],
-      "address": {
-        line1: "123 xy ave",
-        city: "Grand Rapids",
-        "state": "MI",
-        "zip": 458278
-      },
-      "emergencyContact": {
-        "firstName": "bill",
-        "lastName": "clinton"
-      },
-      "pediatrition": [],
-      "restrictions": {
-        "pets": [],
-        "allergies": [],
-        "medications": []
-      },
-      "languages": [],
-      "availability": "PM",
-      "children": [{
-        "firstName": "Tim",
-        "lastName": "Smith",
-        "gender": "Male",
-        "age": 3,
-        "receivingServices": true
-      }, {
-        "firstName": "Billy",
-        "lastName": "Smith",
-        "gender": "Male",
-        "age": 8
-      }]
-    };
+    $scope.mother = $rootScope.mother || $scope.mother || Mother.get({id: $routeParams.id});
+
+    $scope.mother.address = $scope.mother.address || {};
 
 
     $scope.selectedAvailability = _.reduce($scope.availabilityCodes, function(res, av){
-      return  $scope.mother.availability && ((av.value == $scope.mother.availability) ? av : res);
+      return  $scope.mother.availability && ((av.value == $scope.mother.availability) ? av.value : res.value);
     });
 
     $scope.selectedState = _.reduce($scope.states, function(res, st){
@@ -63,7 +26,7 @@ angular.module('webApp')
 
     $scope.update = function() {
       $scope.mother.address.state = $scope.selectedState.abbreviation;
-      $scope.mother.availability = $scope.selectedAvailability.value;
+      $scope.mother.availability = $scope.selectedAvailability;
       $scope.mother.languages = _.map($scope.selectedLanguages, function(l) { return l.abbr });
 
       var mother = new Mother($scope.mother);
