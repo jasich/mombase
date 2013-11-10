ddbf0dec87ceae7a590f0444d6923555341d831d
MRSICH <jasich@gmail.com>
2013-11-10 07:30:51 -0500

    Updates for Mother editing.

--- src/web/app/scripts/controllers/mothers/edit.js
+++ src/web/app/scripts/controllers/mothers/edit.js
@@ -2,9 +2,44 @@
 
 angular.module('webApp')
   .controller('MothersEditCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.mother = {
+      "__v": 16,
+      "_id": "527ee220c2f63cc762000006",
+      "email": "pam@email.com",
+      "firstName": "Pam",
+      "lastName": "Smith",
+      "primaryVolunteer": "",
+      "visits": [],
+      "volunteers": [],
+      "address": {
+        line1: "123 xy ave",
+        city: "Grand Rapids",
+        "state": "MI",
+        "zip": 458278
+      },
+      "emergencyContact": {
+        "firstName": "bill",
+        "lastName": "clinton"
+      },
+      "pediatrition": [],
+      "restrictions": {
+        "pets": [],
+        "allergies": [],
+        "medications": []
+      },
+      "languages": [],
+      "availability": [],
+      "children": [{
+        "firstName": "Tim",
+        "lastName": "Smith",
+        "gender": "Male",
+        "age": 3,
+        "receivingServices": true
+      }, {
+        "firstName": "Billy",
+        "lastName": "Smith",
+        "gender": "Male",
+        "age": 8
+      }]
+    };
   });
--- src/web/app/scripts/services/mothers.js
+++ src/web/app/scripts/services/mothers.js
@@ -2,10 +2,12 @@
 
 angular.module('webApp')
   .factory('Mother', function Mothers($resource) {
-    var resource = $resource("api/mothers/:id/:action",
+    var resource = $resource("api/mothers/:action/:id",
       {id: '@id', action: '@action'},
       {
-        search: {method: 'POST', isArray: true, params: {action: 'search'}}
+        search: {method: 'POST', isArray: true, params: {action: 'search'}},
+        assign: {method: 'POST', isArray: false, params: {action: 'assign', volunteerEmail: "@volunteerEmail"}},
+        unassign: {method: 'POST', isArray: false, params: {action: 'unassign', volunteerEmail: "@volunteerEmail"}}
       });
 
     return resource;
